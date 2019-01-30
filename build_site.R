d96518aef75d971672a706db2d475796a03fa137
masedki <masedki@gmail.com>
2019-01-30 10:46:33 +0100

    mise à jour de l'affiliation

--- build_site.R
+++ build_site.R
@@ -2,8 +2,9 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-#render your sweet site. 
-rmarkdown::render_site()
+
 # actually remove the files
 rmarkdown::clean_site()
+#render your sweet site. 
+rmarkdown::render_site()
 
