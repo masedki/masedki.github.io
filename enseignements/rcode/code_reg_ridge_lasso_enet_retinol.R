b814550f033ddb008f7e61d0fdda85b40a305c7b
masedki <masedki@gmail.com>
2020-02-14 10:50:18 +0100

    ajout des éléments de la séance du 14/02 du cours de data science en M1

--- enseignements/rcode/code_reg_ridge_lasso_enet_retinol.R
+++ enseignements/rcode/code_reg_ridge_lasso_enet_retinol.R
@@ -1,16 +1,10 @@
 rm(list=ls())
 require(lasso2)
 require(MASS)
+retinol = read.csv2("~/codes/masedki/enseignements/datasets/presentationTPretinol.csv")
+summary(retinol)
 # lecture du jeu de données
 data("Prostate")
-summary(Prostate)
-
-# Régression linéaire multiple 
-modlm = lm(lpsa~., Prostate)
-summary(modlm)
-
-
-# Régression ridge
 lam = seq(0.1,200,len=99)
 fit = lm.ridge(lpsa~., Prostate, lambda=lam)
 # chaque courbe (une coleur) correspond à l'évolution de la variable du coefficient de régression d'une variable en fonction de lambda 
@@ -49,15 +43,14 @@ legend("topright",lty=1, lwd=2, col = 1:8, legend = colnames(Prostate[,-9]))
 lam = exp(seq(log(1.5),log(0.001),len=100))
 #un vecteur pour stocker l'erreur de validation croisée correspondant aux meilleur lambda à alpha fixé 
 errcv = rep(NA, 11)
-set.seed(19)
 for(i in 0:10)
 {
- cvfit=cv.glmnet(x, y, family="gaussian", lambda=lam, nfolds=3, alpha = i/10) 
+ cvfit=cv.glmnet(x, y, family="gaussian", lambda=lam, nfolds=5, alpha = i/10) 
  assign(paste("fit", i, sep="."), cvfit)  
  errcv[i+1] = min(cvfit$cvm) 
 }
 ## affichage du meilleur alpha
-print(paste("best alpha : ", (which.min(errcv) - 1)/10, sep="", " minimal error :  ", min(errcv))) 
+print(paste("best alpha : ", (which.min(errcv) - 1)/10, sep="")) 
 #meilleur modèle qui correspond au meilleur alpha et et au meilleur lambda
 cvfitbest  = get(paste("fit", (which.min(errcv) - 1), sep="."))
 ## affichage de la meilleur valeur de lambda
