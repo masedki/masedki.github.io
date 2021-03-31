b4db78b8c5f912a47a4129382d400e97fb3e5a24
sedki <masedki@gmail.com>
2021-03-31 08:19:19 +0200

    màj de sta212

--- enseignements/sta212/tp_intro.R
+++ enseignements/sta212/tp_intro.R
@@ -1,10 +1,7 @@
-# lecture de la librairie qui permet de reproduire une grande partie des exemples du livre : Element of Statistical Learning
-# pour l'installer : install.packages("ElemStatLearn", dep=TRUE)
-require(ElemStatLearn)
-# lecture de la librairie  qui permet d'ajuster des modèles d'apprentissage par les knn
+setwd("~/codes/tp_R/")
+require(MASS)
 require(class)
-# on charge le jeu de données mixture.example sans installer la librairie ElemStatLearn
-data("mixture.example")
+load("mixture.example.rda")
 # 200 données de dimension 2 sous forme de matrice 200 x 2 
 x <- mixture.example$x
 dim(x)
@@ -14,48 +11,37 @@ y <- mixture.example$y
 table(y)
 # on trace le nuage de points avec couleur rouge si le label du point vaut 1 et vert s'il vaut 0. 
 plot(x, col=ifelse(y==1,"red", "green"), xlab="x1", ylab="x2")
+
+
+
+
+
+
+
 # un autre nuage de points sous forme de matrice 6831 x 2 sous forme de grille régulière 
-px1 = mixture.example$px1 # 
+px1 = mixture.example$px1 
 px2 = mixture.example$px2
 xnew = mixture.example$xnew
 dim(xnew)
 # cette grille permet d'obtenir un maillage de l'espace comme suit
 plot(xnew, pch=".",xlab="x1", ylab="x2")
-# cette grille a été obtenue en composant tous les couples qu'on peut former comme suit 
-# première coordonnée de px1 et seconde coordonnée de px2.
 
-# modèle à 15 plus proche voisins basé sur les 200 données observées x et y
-# aide de la fonction knn identifier les différents paramètres d'entrées. 
-?knn 
-########################################################################################
-######################### modèle à 15 plus proches voisins #############################
-########################################################################################
-# les paramètres de la fonction 
-# train : les variables explicatives des données d'entrainement  :  x 
-# test : le jeu de données pour lequel on a envie de prédire la variable réponse à partir des données d'entrainement
-# cl : les classes des individus du jeu de données d'entrainement:  y
-# k : le nombre de voisins du modèle
-# prob = TRUE permet de renvoyer les votes pour prédire la classe 1 de chaque point du jeu de données test
+
+## 15 plus proches voisins
 mod15 = knn(train=x, test=xnew, cl=y, k=15, prob=TRUE)
 # résumé des sorties du modèle mod15
 summary(mod15)
-# les effectifs des prédictions des deux classes 0 et 1 pour le jeu de données test
-# On récupère les proportions de votes de la classe majoritaire pour chaque point du jeu de données test 
+
 prob = attr(mod15, "prob")
-# Les probas de votes de la classe majoritaire 
 prob = ifelse(mod15=="1", prob, 1-prob) 
-# On construit la la matrice qui donne la proprtion de votre de la classe majoritaire pour chaque point de la grille qui est le jeu de données test. 
 prob15 <- matrix(prob, length(px1), length(px2))
-# On trace la frontière de classement où ces proportions vallent 0.5 (là où on bascule d'une classe à une autre)
 contour(px1, px2, prob15, levels=0.5, labels="", xlab="x1", ylab="x2", main="frontière de classement du modèle 15-nn")
 # ajout du points du jeu de données d'apprentissage au modèle
 points(x, col=ifelse(y==1, "red", "green"))
 
 
-######################################################################################
-######################## modèle à 1 plus proches voisins #############################
-######################################################################################
-# On refait exactement les mêmes étapes précédentes 
+
+# 1 plus proche voisin 
 mod1 <- knn(x, xnew, k=1, cl=y, prob=TRUE)
 prob <- attr(mod1, "prob")
 prob <- ifelse( mod1=="1", prob, 1-prob) 
@@ -65,10 +51,7 @@ points(x, col=ifelse(y==1, "red", "green"))
 
 
 
-# Nous remarquons que la frontière de classement du modèle 1-nn est plus fine que celle du modèle 15-nn 
-# La complexité du modèle knn est inversement proportionnelle aux nombre de voisins k 
-# on simule un jeu de données test de taille 10000 avec le même modèle qui a permet d'obtenir les 200 point d'apprentissage x, y
-# le bloc de code suivant permet de simuler le jeu de données test : xtest et ytest
+# simuler le jeu de données test : xtest et ytest
 require(MASS)
 set.seed(123)
 centers = c(sample(1:10, 5000, replace=TRUE), sample(11:20, 5000, replace=TRUE))
@@ -78,9 +61,6 @@ xtest = mvrnorm(10000, c(0,0), 0.2*diag(2))
 xtest = xtest + means
 ytest = c(rep(0, 5000), rep(1, 5000))
 
-
-# Nous allons comparer deux type d'erreurs  comme suit : 
-# Dans les deux cas, nous allons construire un modèle sur le jeu de données d'apprentissage x, y et nous comparons :
 # Erreur de classement du jeu de données d'apprentissage x, y en fonction du nombre de voisins
 # Erreur de classement du jeu de données test xtest, ytest en fonction du nombre de voisins
 
@@ -108,7 +88,8 @@ for (i in 1:nK)
 
 # Figure pour superposer les deux erreurs
 # erreur test 
-plot(K, ErrTest, type="b", col="blue", xlab="nombre de voisins",ylab=" erreurs train et test", pch=20, 
+plot(K, ErrTest, type="b", col="blue", xlab="nombre de voisins",
+     ylab=" erreurs train et test", pch=20, 
      ylim=range(c(ErrTest, ErrTrain)))
 # erreur d'apprentissage
 lines(K, ErrTrain,type="b",col="red",pch=20)
