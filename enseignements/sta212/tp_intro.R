7ffeb5209fdcb84cde299e1d76ba1a518965056a
sedki <masedki@gmail.com>
2023-03-29 07:40:32 +0200

    màj

--- enseignements/sta212/tp_intro.R
+++ enseignements/sta212/tp_intro.R
@@ -1,57 +1,85 @@
-setwd("~/codes/tp_R/")
-require(MASS)
+rm(list=ls())
+setwd("~/Dropbox/enseignement/ensta/TP/R_codes_corrige/")
 require(class)
-load("mixture.example.rda")
-# 200 données de dimension 2 sous forme de matrice 200 x 2 
+load("mixture_example.rda")
 x <- mixture.example$x
 dim(x)
 head(x)
-# les labels des données 0 ou 1 
 y <- mixture.example$y
 table(y)
-# on trace le nuage de points avec couleur rouge si le label du point vaut 1 et vert s'il vaut 0. 
+
+
 plot(x, col=ifelse(y==1,"red", "green"), xlab="x1", ylab="x2")
 
 
 
 
 
-
-
-# un autre nuage de points sous forme de matrice 6831 x 2 sous forme de grille régulière 
 px1 = mixture.example$px1 
 px2 = mixture.example$px2
 xnew = mixture.example$xnew
 dim(xnew)
-# cette grille permet d'obtenir un maillage de l'espace comme suit
+
 plot(xnew, pch=".",xlab="x1", ylab="x2")
-
-
-## 15 plus proches voisins
+ 
+?knn 
 mod15 = knn(train=x, test=xnew, cl=y, k=15, prob=TRUE)
-# résumé des sorties du modèle mod15
+
 summary(mod15)
 
 prob = attr(mod15, "prob")
 prob = ifelse(mod15=="1", prob, 1-prob) 
 prob15 <- matrix(prob, length(px1), length(px2))
-contour(px1, px2, prob15, levels=0.5, labels="", xlab="x1", ylab="x2", main="frontière de classement du modèle 15-nn")
-# ajout du points du jeu de données d'apprentissage au modèle
+contour(px1, px2, prob15, levels=0.5, 
+        labels="0.5", xlab="x1", ylab="x2", 
+        main="frontière de classement du modèle 15-nn")
+
+
+
+mod2 = knn(train=x, test=xnew, cl=y, k=2, prob=TRUE)
+summary(mod2)
+prob = attr(mod2, "prob")
+prob = ifelse(mod2=="1", prob, 1-prob) 
+prob2 <- matrix(prob, length(px1), length(px2))
+contour(px1, px2, prob2, levels=0.5, 
+        labels="0.5", xlab="x1", ylab="x2", 
+        main="frontière de classement du modèle 2-nn")
+
+
+mod180 = knn(train=x, test=xnew, cl=y, k=180, prob=TRUE)
+summary(mod180)
+prob = attr(mod180, "prob")
+prob = ifelse(mod180=="1", prob, 1-prob) 
+prob180 <- matrix(prob, length(px1), length(px2))
+contour(px1, px2, prob180, levels=0.5, 
+        labels="0.5", xlab="x1", ylab="x2", 
+        main="frontière de classement du modèle 180-nn")
 points(x, col=ifelse(y==1, "red", "green"))
 
+probbayes = mixture.example$prob
 
+probbayes <- matrix(probbayes, length(px1), length(px2))
 
-# 1 plus proche voisin 
+contour(px1, px2, probbayes, 
+        levels=0.5, 
+        labels="", xlab="", ylab="",
+        main="",
+        col="blue",
+        lwd=2,
+        add=TRUE)
+
+#points(x, col=ifelse(y==1, "red", "green"))
 mod1 <- knn(x, xnew, k=1, cl=y, prob=TRUE)
 prob <- attr(mod1, "prob")
 prob <- ifelse( mod1=="1", prob, 1-prob) 
 prob1 <- matrix(prob, length(px1), length(px2) )
-contour(px1, px2, prob1, level=0.5, labels="", xlab="x1", ylab="x2", main="frontière de classement du modèle 1-nn")
+contour(px1, px2, prob1, level=0.5, labels="", xlab="x1", ylab="x2", 
+        main="frontière de classement du modèle à 1-nn")
 points(x, col=ifelse(y==1, "red", "green"))
 
 
 
-# simuler le jeu de données test : xtest et ytest
+
 require(MASS)
 set.seed(123)
 centers = c(sample(1:10, 5000, replace=TRUE), sample(11:20, 5000, replace=TRUE))
@@ -61,42 +89,33 @@ xtest = mvrnorm(10000, c(0,0), 0.2*diag(2))
 xtest = xtest + means
 ytest = c(rep(0, 5000), rep(1, 5000))
 
-# Erreur de classement du jeu de données d'apprentissage x, y en fonction du nombre de voisins
-# Erreur de classement du jeu de données test xtest, ytest en fonction du nombre de voisins
 
-# le vecteur suivant contient les valeurs de K à tester : on peut évidemment tester d'autres valeurs
-K = c(1,3,5,7,9,11,15,17,23,25,35,45,55,83,101,151)
-# le nombre de valeurs à tester 
+
+
+#K = c(1,3,5,7,9,11,15,17,23,25,35,45,55,83,101,151)
+K = 1:150
 nK = length(K)
-# deux vecteurs vides pour stocker les deux types d'erreurs 
+
 ErrTrain = rep(NA, length=nK)
 ErrTest  = rep(NA, length=nK)
-# une boucle qui permet de parcourir les valeurs de k 
 for (i in 1:nK) 
   {
-  # valeur courante de k 
   k = K[i] 
-  # modèle pour classer le jeu de données d'apprentissage
+  
   modtrain = knn(x, x, k=k, cl=y)
-  # proportion de mal classés du jeu de données d'apprentissage
   ErrTrain[i] = mean(modtrain!=y)
-  # modèle pour classer le jeu de données de test (appelé aussi validation)
   modtest = knn(x, xtest, k=k, cl=y)
-  # proportion de mal classés du jeu de données de test
   ErrTest[i] = mean(modtest!=ytest)
 }
-
-# Figure pour superposer les deux erreurs
-# erreur test 
-plot(K, ErrTest, type="b", col="blue", xlab="nombre de voisins",
+ 
+plot(K, ErrTest, type="b", col="blue", 
+     xlab="nombre de voisins",
      ylab=" erreurs train et test", pch=20, 
      ylim=range(c(ErrTest, ErrTrain)))
-# erreur d'apprentissage
-lines(K, ErrTrain,type="b",col="red",pch=20)
-# une petite légende
-legend("bottomright",lty=1,col=c("red","blue"),legend = c("train ", "test "))
 
-# Conclusion : 
-# le modèle le plus complèxe est désigné comme le plus performant par l'erreur d'apprentissage
-# c'est le phénomène de sur-apprentissage ou sur-ajustement.
-# l'erreur de test permet de contourner ce phénomène. 
+lines(K, ErrTrain,type="b",col="red",pch=20)
+
+legend("bottomright",lty=1,col=c("red","blue"),
+       legend = c("train ", "test "))
+which.min(ErrTrain)
+which.min(ErrTest)
